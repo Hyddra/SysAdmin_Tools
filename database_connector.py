8d5bc2fc21431e2c3bd26edd1df098bfba9e74d0
hyddra <hyddra@protonmail.com>
2018-11-10 11:38:06 +0000

    stuff

---
+++ database_connector.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+# First thing to do, is to make a connection
+# to the database we want to use
+# Not that that the parameter for the password is 'passwd' and not 'password'
+
+db = MySQLdb.connect(host="localhost", user="hyddra", passwd="!!Styllez00", db="users")
+
+# Create a cursor for the select
+cur = db.cursor()
+
+# execute an sql query
+cur.execute("create table song (id int unsigned primary key auto_increment, title text not null)")
+
+cur.execute("drop table song")
+
+# close the cursor
+cur.close()
+
+# close the connection
+db.close()
