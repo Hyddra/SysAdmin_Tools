0f4cf2926f23ecdad58c3246e20ec5df343c9e79
hyddra <hyddra@localhost.localdomain>
2019-07-14 18:45:08 +0100

    House Keeping

--- bash_bruteforce.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for nr in {0..9999}:
-do
-	echo "<password_here> $nr" | nc localhost 30002
-done
-	
-	
--- ping_sweeper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# write a bash script to ping a range of ip's
-# and inform us which ones reply
-
-for ip in `seq 1 10`; do
-	ping -c 1 10.0.0.$ip | grep '64 bytes' | cut -d " " -f 4
-done
