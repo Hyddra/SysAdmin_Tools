fbbfdf70d19a2eec320c0bb526477a349b252da4
hyddra <rmatosgil@gmail.com>
2019-08-12 00:59:11 +0100

    Automating web browsing

--- Glastonbury.py
+++ Glastonbury.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
 
 from selenium import webdriver
-import webbrowser
-
 import time
 
-print("We are automating this shit")
-url=input("url to open: ")
 
-# This is not bad, but we want to handle the refresh and not extra tabs
-# it can be a real pain to manage the dozens of tabs spawn per min
+# 'http://www.glastonbury.seetickets.com'
 
-'''
-if url == '':
-    url = 'http://www.glastonbury.seetickets.com'
-else:
-    webbrowser.open(url)
-    '''
+# ------------------------------------------------------------------#
+address = input("Where to search > ")
+refresh = input("want to choose the refresh rate? [y/n]: ").lower()
+
+refresh_rate = 0
+# ------------------------------------------------------------------#
+
+print("Automating money spending....")
+# address = 'http://www.glastonbury.seetickets.com'
+
+
+if not address:
+    address = "https://www.glastonbury.seetickets.com"
+if refresh == 'y':
+    refresh_rate = int(input("How many seconds between refresh? "))
 
 browser = webdriver.Firefox()
-browser.get(url)
+browser.get(address)
 
-# Refresh the page?!
-driver.refresh()
-
-
-    #webbrowser.open(url)
-
-    # <insert> vpn hoping to avoid being detected as spam?
-    # it is possible that the ip is locked, and by hoping we end up getting back to the beginning of the fkn queue
-    # What about 10 machines doing the same shit and reporting back when sucessful login?
-
-time.sleep(2)
+while True:
+    time.sleep(refresh_rate)
+    browser.refresh()
 
