dfd7d5f205e454fe23f3a2ff4d64e98561ec6b75
hyddra <hyddra@localhost.localdomain>
2020-03-26 22:35:59 +0000

    Shrinker.sh - rev. 1.1 One day I will add validation to the list....

--- monitoring/shrinker.sh
+++ monitoring/shrinker.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 # ================================================================
-# Title             : <need a cool name>
-# Descripion        : Compresses files older than +365 days 
-# Author            : 
-# Date              : 2020-03-26
-# Revision / QA     : 
-# Version           : 1.0
-# Usage             : bash <script.sh>
-# Bash_Version      : 3.2.25(1)-release 
+# Title             : shrinker.sh
+# Descripion        : Compresses files older than 365 days 
+# Author            : Hyddra
+# Date              : 2020/03/26
+# Version           : 1.1
+# Usage             : bash shrinker.sh
+# Bash_Version      : 5.0.11(1)-release
 # ================================================================
 
 # We are hard-coding a list with the worst offenders ( last update - 2020/03/26 )
 
-deposit="/tmp/"
+deposit="/tmp"
+directories=("home" "mnt" "opt" "usr" "var")
 
 if [[ $(pwd) != $deposit ]]; then cd $deposit; fi
 
-echo -e "\nThese are the directories currently hard-coded:"
+
+echo -e "\nAs of $(date +%Y/%m/%d), the worst offenders are: "
+# Sure, smartly expand the list but don't validate if the list is still accurate... :)
+
 for i in ${directories[@]}; do echo $i; done
+
 echo -e "\nTotal size BEFORE compression: \n"
 
 # Main logic
-
 for dir in $deposit/*; do
     echo "$(du -s $dir)"
 done
@@ -30,7 +33,6 @@ done
 echo -e "\nTotal size AFTER compression: \n"
 
 for dir in $deposit/*; do
-    #find $dir ! -name "*.gz" -mtime +365 -type f -exec gzip {} \;
     find $dir ! -name "*.gz" -mtime +365 -type f -exec gzip {} \;
     echo "$(du -s $dir)"
 done
